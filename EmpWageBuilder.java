c5fb962bc04475c76f914ffc22ecec36e45534e9
Mohit Shah <mohitshah7777@gmail.com>
2021-04-03 22:57:29 +0530

    Program has been implemented using class methods

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -6,39 +6,45 @@ public class EmpWageBuilder {
 	public static final int NUM_OF_WORKING_DAYS = 20;
 	public static final int MAX_HRS_IN_MONTH = 100;
 	
-	public static void main(String[] args) {
-	
-	int empHrs=0;
-	int empWage=0;
-	int totalEmpHrs=0;
-	int totalWorkingDays=0;
-	//int totalMonthlyEmpWage=0;
-	while(totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS)
+	public static int computeEmpWAGE()
 	{
-		totalWorkingDays++;
-		int empCheck = (int)Math.floor(Math.random() * 10)% 3;
-		switch (empCheck)
+		int empHrs=0;
+		int empWage=0;
+		int totalEmpHrs=0;
+		int totalWorkingDays=0;
+		//int totalMonthlyEmpWage=0;
+		while(totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS)
 		{
-			case IS_FULL_TIME :
-				System.out.println("Employee is Present");
-				empHrs=8;
-				break;
-			case IS_PART_TIME :
-				System.out.println("Part time Employee Present");
-				empHrs=4;
-				break;
-			default:
-				System.out.println("Employee is Absent");
-				empHrs=0;
-				break;
+			totalWorkingDays++;
+			int empCheck = (int)Math.floor(Math.random() * 10)% 3;
+			switch (empCheck)
+			{
+				case IS_FULL_TIME :
+					System.out.println("Employee is Present");
+					empHrs=8;
+					break;
+				case IS_PART_TIME :
+					System.out.println("Part time Employee Present");
+					empHrs=4;
+					break;
+				default:
+					System.out.println("Employee is Absent");
+					empHrs=0;
+					break;
+			}
+			totalEmpHrs+=empHrs;
+			empWage=empHrs*EMP_RATE_PER_HOUR;
+			//totalMonthlyEmpWage+=empWage;
+			System.out.println("Day = " +totalWorkingDays+ " Employee Hour = " +empHrs);
 		}
-		totalEmpHrs+=empHrs;
-		empWage=empHrs*EMP_RATE_PER_HOUR;
-		//totalMonthlyEmpWage+=empWage;
-		System.out.println("Employee WAGE = " +empWage);
+		int totalEmpWage=totalEmpHrs*EMP_RATE_PER_HOUR;
+		System.out.println("Total Employee wage = " +totalEmpWage);
+		//System.out.println("Total Monthly Wage = " +totalMonthlyEmpWage);
+		return totalEmpWage;
 	}
-	int totalEmpWage=totalEmpHrs*EMP_RATE_PER_HOUR;
-	System.out.println("Total Employee wage = " +totalEmpWage);
-	//System.out.println("Total Monthly Wage = " +totalMonthlyEmpWage);
+
+	public static void main(String[] args) {
+
+		computeEmpWAGE();
 	}
 }
