8f25871eb6d08b5f4128cb38e96c45189db5d0fd
Mohit Shah <mohitshah7777@gmail.com>
2021-04-05 21:10:31 +0530

    Program has modified to save employee wages for each company

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -2,14 +2,26 @@ public class EmpWageBuilder {
 	
 	public static final int IS_FULL_TIME =1;
 	public static final int IS_PART_TIME =2;
+
+	private final String company;
+	private final int EMP_RATE_PER_HOUR;
+	private final int NUM_OF_WORKING_DAYS;
+	private final int MAX_HRS_IN_MONTH;
+	private int totalEmpWage;
 	
-	public static int computeEmpWAGE(String company, int EMP_RATE_PER_HOUR, int NUM_OF_WORKING_DAYS, int MAX_HRS_IN_MONTH)
+	public EmpWageBuilder(String company,int EMP_RATE_PER_HOUR, int NUM_OF_WORKING_DAYS,int MAX_HRS_IN_MONTH)
+	{
+		this.company=company;
+		this.EMP_RATE_PER_HOUR=EMP_RATE_PER_HOUR;
+		this.NUM_OF_WORKING_DAYS=NUM_OF_WORKING_DAYS;
+		this.MAX_HRS_IN_MONTH=MAX_HRS_IN_MONTH;
+	}
+	
+	public void computeEmpWAGE()
 	{
 		int empHrs=0;
-		//int empWage=0;
 		int totalEmpHrs=0;
 		int totalWorkingDays=0;
-		//int totalMonthlyEmpWage=0;
 		while(totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS)
 		{
 			totalWorkingDays++;
@@ -32,16 +44,25 @@ public class EmpWageBuilder {
 			totalEmpHrs+=empHrs;
 			System.out.println("Day = " +totalWorkingDays+ " Employee Hour = " +empHrs);
 		}
-		int totalEmpWage=totalEmpHrs*EMP_RATE_PER_HOUR;
-		System.out.println("Total Employee wage for company = "+company+" is = "+totalEmpWage);
-		return totalEmpWage;
+		totalEmpWage=totalEmpHrs*EMP_RATE_PER_HOUR;
 	}
 
-	
+	@Override
+	public String toString()
+	{
+		return "Total Employee wage for company = "+company+" is = "+totalEmpWage;
+	}
+
 	public static void main(String[] args) {
 
-		computeEmpWAGE("Dmart",20,20,100);
-		computeEmpWAGE("Mahindra",40,10,200);
-		computeEmpWAGE("Accenture",20,100,50);
+		EmpWageBuilder Dmart=new EmpWageBuilder("Dmart", 20, 20, 100);
+		EmpWageBuilder Mahindra=new EmpWageBuilder("Mahindra", 40, 10, 200);
+		EmpWageBuilder Accenture=new EmpWageBuilder("Accenture", 20, 100, 50);
+		Dmart.computeEmpWAGE();
+		System.out.println(Dmart);
+		Mahindra.computeEmpWAGE();
+		System.out.println(Mahindra);
+		Accenture.computeEmpWAGE();
+		System.out.println(Accenture);
 	}
 }
